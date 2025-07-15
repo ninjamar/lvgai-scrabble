7c39e031334a6a647fd42eee3f4e98ae8de5747e
ninjamar <70116773+ninjamar@users.noreply.github.com>
2025-07-15 11:23:37 -0500

    Add time benchmark in redis demo

--- ram/redis_demo.py
+++ ram/redis_demo.py
@@ -1,5 +1,5 @@
 import asyncio
-
+import time
 import redis.asyncio as aredis
 
 from .scrabble import Board, Player, WordList, create_tile_bag
@@ -10,11 +10,19 @@ rd = aredis.Redis(host="ai.thewcl.com", port=6379, db=4, password="atmega328")
 async def main():
     word_list = WordList.load_word_list()
 
-    b = Board(players=[Player()], tile_bag=create_tile_bag())
+    b = Board(players=[Player(), Player()], tile_bag=create_tile_bag())
     b.initialize(word_list)
 
+
+    now = time.time()
     await b.save_to_redis()
+    old = time.time() - now
+    print("Save:", old)
+
+    now = time.time()
     c = await b.load_from_redis(word_list=word_list)
+    old = time.time() - now
+    print("Load:", old)
 
     assert c == b
 
